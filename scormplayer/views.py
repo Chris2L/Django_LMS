4754d0f016894aea9709beefb8bf67edac7527a5
Christo Labuschagne <fc.labuschagne@gmail.com>
2023-11-27 00:34:45 +0200

    Playing around with some of the SCORMS to see what they return Trying to figure out what I would want to save

--- scormplayer/views.py
+++ scormplayer/views.py
@@ -6,19 +6,19 @@ from icecream import ic
 
 # Create your views here.
 def index(request):
-     context = {
-          'lesson_location': 3,
-          'total_time': '01:17:45.05',
-          'student_id': "user1",
-          'student_name': "John Doe",
-          'lesson_status': 'incomplete', # completed passed incomplete
-          'score': json.dumps({
-                    "raw": '70',
-                    "min": '0',
-                    "max": '100'
-                    }),
-     }
-     return render(request,'scorm_player_12.html', context=context)
+    context = {
+         'lesson_location': 3,
+         'total_time': '01:17:45.05',
+         'student_id': "user1",
+         'student_name': "John Doe",
+         'lesson_status': 'incomplete', # completed passed incomplete
+         'score': json.dumps({
+                   "raw": '70',
+                   "min": '0',
+                   "max": '100'
+                   }),
+    }
+    return render(request,'scorm_player_12.html', context=context)
 
 
 def save_scorm(request):
@@ -34,20 +34,29 @@ def save_scorm(request):
 
 
 def index_2004(request):
-     context = {
-          'lesson_location': 3,
-          'student_id': "user1",
-          'student_name': "John Doe",
-          'lesson_status': 'incomplete', # completed passed incomplete
-          'scaled_passing_score': 0.5,
-          'score': json.dumps({
-                    "raw": '70',
-                    "min": '0',
-                    "max": '100',
-                    "scaled": '0.7'
-                    }),
-     }
-     return render(request,'scorm_player_2004.html', context=context)
+    context = {
+        # 'course_path': '/media/courses/PlayTheGame_2004/RuntimeBasicCalls_SCORM20043rdEdition/shared/launchpage.html',
+        # 'course_path': '/media/courses/PlayTheGame_2004/RunTimeAdvancedCalls_SCORM20043rdEdition/shared/launchpage.html',
+        'course_path': '/media/courses/PlayTheGame_2004/aca_2022_scorm2004_passed-incomplete_en-us/index_lms.html',
+        'lesson_location': 3,
+        'student_id': "user1",
+        'student_name': "John Doe",
+        'lesson_status': 'incomplete', # completed passed incomplete
+        'scaled_passing_score': 0.5,
+        'score': json.dumps({
+                  "raw": '70',
+                  "min": '0',
+                  "max": '100',
+                  "scaled": '0.7'
+                  }),
+        #  'objectives': json.dumps({
+        #        '0': {'id': 'obj_etiquette',},
+        #        '1': {'id': 'obj_handicapping',},
+        #        '2': {'id': 'obj_havingfun',},
+        #        '3': {'id': 'obj_playing',},
+        #  })
+    }
+    return render(request,'scorm_player_2004.html', context=context)
 
 
 def save_scorm_2004(request):
@@ -62,6 +71,50 @@ def save_scorm_2004(request):
     ic(json_object["cmi"]['score']['scaled'])
 
     ic(json_object["cmi"]['success_status']) # unknown / failed / passed
+
+    # Use this data just not print for now
+    # ic(json_object["cmi"]['suspend_data'])
+
+    try:
+        ic(json_object["cmi"]['progress_measure'])
+    except:
+        ic("No progress_measure")
+
+
+    try:
+        for interaction in json_object["cmi"]['interactions']:
+            try:
+                ic(f"========== Interaction {interaction} ===============")
+                ic(json_object["cmi"]['interactions'][interaction]["id"])
+                ic(json_object["cmi"]['interactions'][interaction]["learner_response"])
+                ic(json_object["cmi"]['interactions'][interaction]["latency"])
+                ic(json_object["cmi"]['interactions'][interaction]["objectives"])
+                ic(json_object["cmi"]['interactions'][interaction]["result"])
+                ic(json_object["cmi"]['interactions'][interaction]["weighting"])
+
+            except:
+                ic(f"error for interactions[{interaction}]")
+    except:
+        ic("no interaction")
+
+    try:
+        for objective in json_object["cmi"]['objectives']:
+            try:
+                ic(f"========== Objective {objective} ===============")
+                ic(json_object["cmi"]['objectives'][objective]["id"])
+                ic(json_object["cmi"]['objectives'][objective]["completion_status"])
+                ic(json_object["cmi"]['objectives'][objective]["progress_measure"])
+                ic(json_object["cmi"]['objectives'][objective]["success_status"])
+                ic(json_object["cmi"]['objectives'][objective]["success_status"])
+                ic(json_object["cmi"]['objectives'][objective]['score']['min'])
+                ic(json_object["cmi"]['objectives'][objective]['score']['max'])
+                ic(json_object["cmi"]['objectives'][objective]['score']['raw'])
+                ic(json_object["cmi"]['objectives'][objective]['score']['scaled'])
+
+            except:
+                ic("objective[id]")
+    except:
+        ic("no objectives")
 
 #     json_formatted_str = json.dumps(json_object, indent=2)
 
