f2227396210fe1c0feb31c42f25b9ecf12a8e9bc
Christo Labuschagne <fc.labuschagne@gmail.com>
2024-01-26 21:14:02 +0200

    Fixed static path

--- core/urls.py
+++ core/urls.py
@@ -25,6 +25,8 @@ urlpatterns = []
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, show_indexes=True)
+    
 urlpatterns += [
     path("", include("scormplayer.urls")),
     path("", include("lms.urls")),
